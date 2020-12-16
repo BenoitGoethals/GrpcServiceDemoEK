9f45b15cdc155151f859a2541da0eeca7d263bba
benoit <benoit.goethals@gmail.com>
2020-12-16 09:29:18 +0100

    test

--- ChatCommonTests/model/ChatRoomTests.cs
+++ ChatCommonTests/model/ChatRoomTests.cs
@@ -12,33 +12,19 @@ namespace ChatCommonTests.model
         public void AddMessageTest()
         {
             ChatRoom chatRoom = new ChatRoom();
-            var chatter = new Chatter() { Guid = Guid.NewGuid(), Enabled = true, Name = "benoit" };
-            var chatter1 = new Chatter() { Guid = Guid.NewGuid(), Enabled = true, Name = "ilse" };
-            var chatter2 = new Chatter() { Guid = Guid.NewGuid(), Enabled = true, Name = "liv" };
-            chatRoom.AddChatter(chatter);
-            chatRoom.AddChatter(chatter1);
-            chatRoom.AddChatter(chatter2);
+            
 
 
-            chatRoom.AddMessage(new Message(){ChatRoom=chatRoom,Content="loilsel",Id=Guid.NewGuid(),Chatter=chatter2});
-            chatRoom.AddMessage(new Message() { ChatRoom = chatRoom, Content = "loilselfdffs", Id = Guid.NewGuid(), Chatter = chatter2 });
-            chatRoom.AddMessage(new Message() { ChatRoom = chatRoom, Content = "loilsedsfdsfdsfdsfdsl", Id = Guid.NewGuid(), Chatter = chatter2 });
-            chatRoom.AddMessage(new Message() { ChatRoom = chatRoom, Content = "loilsedsfdsfdsfdsfdsl", Id = Guid.NewGuid(), Chatter = chatter });
-            chatRoom.AddMessage(new Message() { ChatRoom = chatRoom, Content = "ddddd", Id = Guid.NewGuid(), Chatter = chatter });
+            chatRoom.AddMessage(new Message(){ChatRoom="sdfdsfds",Content="loilsel",Id=Guid.NewGuid(),Chatter="dsfdsfds"});
+            chatRoom.AddMessage(new Message() { ChatRoom = "sdfsdf", Content = "loilselfdffs", Id = Guid.NewGuid(), Chatter = "sdfds" });
+            chatRoom.AddMessage(new Message() { ChatRoom = "sdfsdf", Content = "loilsedsfdsfdsfdsfdsl", Id = Guid.NewGuid(), Chatter = "sdfds" });
+            chatRoom.AddMessage(new Message() { ChatRoom = "sdfsdf", Content = "loilsedsfdsfdsfdsfdsl", Id = Guid.NewGuid(), Chatter = "sdfds" });
+            chatRoom.AddMessage(new Message() { ChatRoom = "sdfds", Content = "ddddd", Id = Guid.NewGuid(), Chatter = "sdfdsf" });
             chatRoom.GetAllMessages().Count.Should().Be(5);
-            chatRoom.GetAllMessages().Count(i => i.Chatter.Equals(chatter2)).Should().Be(3);
-            chatRoom.GetAllMessages().Count(i => i.ChatRoom.Equals(chatRoom)).Should().Be(5);
+            chatRoom.GetAllMessages().Count(i => i.Chatter.Equals("sdfsdf")).Should().Be(0);
+        //    chatRoom.GetAllMessages().Count(i => i.ChatRoom.Equals("sdfds")).Should().Be(3);
         }
 
 
-        [Fact()]
-        public void AddChatterTest()
-        {
-            ChatRoom chatRoom = new ChatRoom();
-            chatRoom.AddChatter(new Chatter() { Guid = Guid.NewGuid(), Enabled = true, Name = "benoit" });
-            chatRoom.AddChatter(new Chatter() { Guid = Guid.NewGuid(), Enabled = true, Name = "ilse" });
-            chatRoom.AddChatter(new Chatter() { Guid = Guid.NewGuid(), Enabled = true, Name = "liv" });
-            chatRoom.GetChatters().Count.Should().Be(3);
-        }
     }
 }
