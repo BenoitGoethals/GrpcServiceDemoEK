34b5caf3eb1fadf9591b3ba31a48e7cc1dfa33ae
benoit <benoit.goethals@gmail.com>
2020-12-23 22:55:19 +0100

    refactor test

--- GrpcServiceBookTests/Services/BookServiceTests.cs
+++ GrpcServiceBookTests/Services/BookServiceTests.cs
@@ -1,5 +1,7 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using GrpcServiceBook;
 using Xunit;
 using XUnitTestProjectServiceUsers.Fixture;
@@ -27,6 +29,14 @@ namespace GrpcServiceBookTests.Services
         {
             var client = new LibBook.LibBookClient(_channel);
             var rt = client.GetBook(new RequestIsbn() {Isbn = "16190610 7261"}).Id.Should().Be(2);
+        }
+        [Fact()]
+        public void GetBookBqdTest()
+        {
+            var client = new LibBook.LibBookClient(_channel);
+            client.Invoking(y => y.GetBook(new RequestIsbn()))
+                .Should().Throw<RpcException>();
+
         }
     }
 }
