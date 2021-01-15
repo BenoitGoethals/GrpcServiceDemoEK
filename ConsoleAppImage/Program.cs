37a7ddfe464fdceb727860fcf8fcad916f78df1d
benoitgoethals <benoit.goethals@mil.be>
2021-01-15 11:34:52 +0100

    update

--- ConsoleAppImage/Program.cs
+++ ConsoleAppImage/Program.cs
@@ -16,64 +16,39 @@ namespace ConsoleAppImage
             Console.WriteLine("Hello send!");
             try
             {
-              CancellationTokenSource cancellationTokenSource=new CancellationTokenSource();  
-              var stream=  File.ReadAllBytes("grpc-basics.png");
-             
-              var channel = GrpcChannel.ForAddress("https://localhost:5001");
-              Images.ImagesClient client=new Images.ImagesClient(channel);
-              //var call = client.addImage();
-              //await call.RequestStream
-              //    .WriteAsync(new UploadFileRequest() { Info=new GrpcServiceImages.Protos.FileInfo(){Name = "c:/temp/grpc-basics.png",Size = stream.Length } });
-              //  foreach (var b in (stream))
-              //{
-              //    await call.RequestStream
-              //        .WriteAsync(new UploadFileRequest() {ChunkData = ByteString.CopyFromUtf8(b.ToString())});
+                CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                var stream = File.ReadAllBytes("grpc-basics.png");
 
-              //}
+                var channel = GrpcChannel.ForAddress("https://localhost:5001");
+                Images.ImagesClient client = new Images.ImagesClient(channel);
 
 
 
+                var call = client.addImageBulk();
+                await call.RequestStream
+                    .WriteAsync(new UploadFileRequest() { Info = new GrpcServiceImages.Protos.FileInfo() { Name = "grpc-basics.png", Size = stream.Length } });
+                foreach (var b in (stream))
+                {
+                    await call.RequestStream
+                        .WriteAsync(new UploadFileRequest() { ChunkData = ByteString.CopyFromUtf8(b.ToString()) });
 
-              //await call.RequestStream.CompleteAsync();
-              //var resp = await call;
-              //Console.WriteLine(resp.Msg);
+                }
 
+                await call.RequestStream
+                    .WriteAsync(new UploadFileRequest() { Info = new GrpcServiceImages.Protos.FileInfo() { Name = "grpc-basics2.png", Size = stream.Length } });
+                foreach (var b in (stream))
+                {
+                    await call.RequestStream
+                        .WriteAsync(new UploadFileRequest() { ChunkData = ByteString.CopyFromUtf8(b.ToString()) });
 
-
-             var call = client.addImageBulk();
-              await call.RequestStream
-                  .WriteAsync(new UploadFileRequest() { Info = new GrpcServiceImages.Protos.FileInfo() { Name = "grpc-basics.png", Size = stream.Length } });
-              foreach (var b in (stream))
-              {
-                  await call.RequestStream
-                      .WriteAsync(new UploadFileRequest() { ChunkData = ByteString.CopyFromUtf8(b.ToString()) });
-
-              }
-              await call.RequestStream
-                  .WriteAsync(new UploadFileRequest() { Info = new GrpcServiceImages.Protos.FileInfo() { Name = "grpc-basics2.png", Size = stream.Length } });
-              foreach (var b in (stream))
-              {
-                  await call.RequestStream
-                      .WriteAsync(new UploadFileRequest() { ChunkData = ByteString.CopyFromUtf8(b.ToString()) });
-
-              }
+                }
 
 
 
                 await call.RequestStream.CompleteAsync();
-               
-            var   resp = await call;
-              Console.WriteLine(resp.Msg);
 
-
-
-
-
-
-
-
-
-
+                var resp = await call;
+                Console.WriteLine(resp.Msg);
 
 
 
